2ba728a9b1790b646d6692840b3ba250d3676b55
Chia-Yang Sun <triumph.sun@gmail.com>
2021-10-07 23:57:15 +0800

    Update DemoController.

--- server/src/main/java/com/suntri/demo/web/controller/DemoController.java
+++ server/src/main/java/com/suntri/demo/web/controller/DemoController.java
@@ -3,7 +3,9 @@ package com.suntri.demo.web.controller;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,11 +24,11 @@ public class DemoController {
     })
     @ResponseBody
     @RequestMapping(value = {"/api/echo"}, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public static Map<String, String> echo(
+    public static ResponseEntity<Map<String, String>> echo(
             @Parameter()  @RequestParam(name="name", required = true) String name) {
         Map<String, String> result = new HashMap<>();
         result.put("name", name);
         result.put("timestamp", String.valueOf(System.currentTimeMillis()));
-        return result;
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
